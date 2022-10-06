88fdb4814ccbccb86e0a94be82c50345685ba8ee
Kenji <kenjiguillen100@gmail.com>
2022-10-06 14:50:01 -0500

    Homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -2,7 +2,7 @@
 
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
-  // Tu código:
+  // Tu código: Si que mas dime algo que no sepa
 }
 
 
