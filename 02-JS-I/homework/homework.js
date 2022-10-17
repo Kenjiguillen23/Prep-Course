f26340f488efa542f73c9ff2853539e5416dd853
Kenji <kenjiguillen100@gmail.com>
2022-10-17 00:13:20 -0500

    Otra prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = hola;
+const nuevoNum = 234;
 
 // Crea una variable booleana:
 const nuevoBool = null;
@@ -13,7 +13,7 @@ const nuevoBool = null;
 const nuevaResta = 10 - null === 2;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
